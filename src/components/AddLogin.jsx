afbfc30df9dc4ec8fed4be7b3d60bda55fd88b6e
Snehatk2002 <sammu5285@gmail.com>
2024-06-13 21:34:44 +0530

    updates

---
+++ src/components/AddLogin.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+const AddLogin = () => {
+    const [data, setdata] = useState(
+        {
+            "email":" ",
+            "password":" "
+        }
+    )
+    const inputHandler=(event)=>{
+        setdata({ ...data,[event.target.name]: event.target.value})
+      }
+      const readValue=()=>{
+          console.log(data)
+      }
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">EMAIL ID</label>
+                            <input type="text" className="form-control" name='email' value={data.email} onChange={inputHandler} />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">PASSWORD</label>
+                            <input type="password" name="password" id="" className="form-control" value={data.password} onChange={inputHandler}/>
+                        </div>
+                        <center>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button class="btn btn-success" onClick={readValue}>LOGIN</button>
+                        </div>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddLogin
