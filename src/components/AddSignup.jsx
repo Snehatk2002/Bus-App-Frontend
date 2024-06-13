321062e76ebc1992a2c0cf045c189fe3113045ca
Snehatk2002 <sammu5285@gmail.com>
2024-06-13 23:25:44 +0530

    updates

--- src/components/AddSignup.jsx
+++ src/components/AddSignup.jsx
@@ -76,6 +76,7 @@ const AddSignup = () => {
                     <div>
                     <center>
                     <Link class="nav-link" to="/">LOGIN</Link>
+                    <Link class="nav-link" to="/add">ADD BUSES</Link>
                     </center>
                     </div>
                 </div>
